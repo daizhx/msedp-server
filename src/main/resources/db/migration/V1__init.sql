a4bc0d1620d0eb8434850e30541a0aa31f2646c2
daizhx <zhixindai@gmail.com>
2019-08-21 07:51:31 +0800

    添加了数据库版本自动同步功能

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,17 +1,29 @@
+CREATE DATABASE IF NOT EXISTS edp;
+
+USE edp;
+
 -- 用户表
 CREATE TABLE IF NOT EXISTS edp_user(
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
   user_name VARCHAR(20) NOT NULL COMMENT '用户名',
-  pwd VARCHAR(60) NOT NULL,
-  birthday DATE DEFAULT NULL,
   tel VARCHAR(20) DEFAULT NULL COMMENT '手机号',
   gender TINYINT DEFAULT NULL COMMENT '性别：0-女,1-男',
-  idcardnumbers char(30) DEFAULT NULL COMMENT '身份证号码',
-  wechatname VARCHAR(20) DEFAULT NULL,
-  home_addr VARCHAR(30) DEFAULT NULL,
-  medicarenumbers VARCHAR(30) DEFAULT NULL,
-  visitinghospital VARCHAR(30) DEFAULT NULL,
-  isSmoking BOOLEAN DEFAULT NULL,
-  smoking_year TINYINT DEFAULT NULL,
+  birthday DATETIME DEFAULT NULL COMMENT '出生日期',
+  citizen_id varchar(30) DEFAULT NULL COMMENT '身份证号码',
+  nick_name VARCHAR(20) DEFAULT NULL COMMENT '昵称',
+  email VARCHAR(30) DEFAULT NULL COMMENT '邮箱',
   PRIMARY KEY (id)
   );
+
+-- edp使用参数设置表
+CREATE TABLE IF NOT EXISTS edp_use_record(
+  id int NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  freq TINYINT NOT NULL COMMENT '频率',
+  duration TINYINT NOT NULL COMMENT '时间',
+  times_per_min TINYINT NOT NULL COMMENT '起搏次数',
+  strength TINYINT NOT NULL COMMENT '强度',
+  start_time DATETIME NOT NULL COMMENT '每次启动治疗时间',
+  user_id int COMMENT '用户id',
+  dev_uuid varchar(64) DEFAULT NULL COMMENT '设备id',
+  PRIMARY KEY (id)
+  )
